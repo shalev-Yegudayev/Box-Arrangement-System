ba82aaa0800636b2f09c80082c6490980d0f3ba8
shalev-Yegudayev <101629222+shalev-Yegudayev@users.noreply.github.com>
2022-05-09 17:09:18 +0300

    Delete BoxArrangementSystemFinal directory

--- BoxArrangementSystemFinal/MainWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace BoxArrangementSystemFinal
-{
-    public partial class MainWindow : Window
-    {
-        public MainWindow() => InitializeComponent();
-    }
-}
